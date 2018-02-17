1ec27699684eb21c2ac1dce25e7a601cfec5bd67
Nicco Engbeck <nengbeck@csumb.edu>
2018-02-17 04:31:45 +0000

    Lab2

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -3,32 +3,22 @@
     <head>
         <title> 777 Slot Machine </title>
         <meta charset="utf-8" />
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
 
+<div id="main">
     <?php
-    for($i=1;$i<5;$i++)
-    {
-        ${"randomValue" . $i } = rand(0,2);
-        displaySymbol(${"randomValue" . $i} );
-    }
-    
-    function displaySymbol($randomValue)
-    {
-    switch($randomValue)
-    {
-        case 0: $symbol = "seven";
-                break;
-        case 1: $symbol = "cherry";
-                break;
-        case 2: $symbol = "lemon";
-                break;
-    }
-    echo "<img src='img/$symbol.png' alt='$symbol' title='symbol' width='70' >";
-    
-    }
+    include 'inc/functions.php';
+    play();
     ?>
-      
-
+    
+    <form>
+        <input type="submit" value="Spin!"/>
+    </form>  
+    
+</div>
     </body>
 </html>
