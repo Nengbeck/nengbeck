35d17e0d462812f92f515f17e7bef54337a0aaac
Nicco Engbeck <nengbeck@csumb.edu>
2018-03-03 05:35:40 +0000

    Lab4

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,80 +1,90 @@
 <?php
 
- //print_r($_GET); //displaying all content submitted in the form using the GET method
-
   $backgroundImage = "img/sea.jpg";
     
-  if (isset($_GET['keyword'])) { //if form was submitted
-      
+  if (isset($_GET['keyword'])) { 
       include 'api/pixabayAPI.php';
-      
+      $keyword = $_GET['keyword'];
       echo "<h3>You searched for " . $_GET['keyword'] . "</h3>";
-      
       $imageURLs = getImageURLs($_GET['keyword']);
-      
-      //print_r($imageURLs);
-      
-      //$backgroundImage = $imageURLs[rand(0, count($imageURLs) - 1)];
       $backgroundImage = $imageURLs[array_rand($imageURLs)];
   }  
-        
- 
-
 ?>
+
 
 <!DOCTYPE html>
 <html>
     <head>
         <title> Lab 4: Pixabay Carousel </title>
+        <meta charset= "utf-8">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="systelsheet">
+        
+        <style>
+            @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+            @import url("css/styles.css");
+            body 
+            {
+                    background-image: url(<?=$backgroundImage?>);
+            }
+            #carouselExampleControls
+            {
+             width: 500px;
+             margin: 0 auto;
+            }
+        </style>
     </head>
-    <style>
-        @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
-        @import url("css/styles.css");
-        body {
-            background-image: url(<?=$backgroundImage?>);
-        }
-        #carouselExampleControls{
-         width: 500px;
-         margin: 0 auto;
-        }
-         
-    </style>
     <body>
-
+        <br>
         <?php
+        $i = rand(0,25);
         
-            if (!isset($_GET['keyword'])) {
-        
-              echo "<h2> You must type a keyword or select a category </h2>";
-            
-            }  
+            if(!isset($imageURLs)){
+                echo "<h2>Type a keyword to display a slideshow with random images from Pixabay.</h2>";
+            } else {
         ?>
-        
-        <form method="GET">
-            
-            <input type="text" size="20" name="keyword" placeholder="Keyword to search for"/>
-            
-            <input type="submit" value="Submit!"/>
-                    
-        </form>
         
         <?php
         if(isset($_GET['keyword'])) {
         ?>
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+              </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
+              <img class="d-block w-100" src="<?=$imageURLs[0+$i]?>" alt="First slide">
             </div>
+            
             <div class="carousel-item">
-              <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
+              <img class="d-block w-100" src="<?=$imageURLs[1+$i]?>" alt="Second slide">
             </div>
+            
             <div class="carousel-item">
-              <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+              <img class="d-block w-100" src="<?=$imageURLs[2+$i]?>" alt="Third slide">
             </div>
+            
              <div class="carousel-item">
-              <img class="d-block w-100" src="<?=$imageURLs[3]?>" alt="Third slide">
+              <img class="d-block w-100" src="<?=$imageURLs[3+$i]?>" alt="fourth slide">
             </div>
+            
+             <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[4+$i]?>" alt="fifth slide">
+            </div>
+            
+             <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[5+$i]?>" alt="sixth slide">
+            </div>
+            
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[6+$i]?>" alt="seventh slide">
+            </div>
+            
           </div>
           <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -88,8 +98,37 @@
         <?php
         }
         ?>
+        
+        <?php
+           }
+        ?>
+        <br>
+        <!-- //////////////////////////////// -->
+        
+        <form method="GET">
+            <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
+            <input type="radio" id="lhorizontal" name="layout" value="horizontal"/>
+            <label for="Horizontal"></label><label for="lhorizontal"> Horizontal </label>
+            <input type="radio" id="lvertical" name="layout" value="vertical">
+            <label for="Vertical"></label><label for="lvertical"> Vertical </label>
+            
+            <select name="category">
+                <option value ="">Select One</option>
+                <option value="sea" name="keyword">Sea</option>
+                <option value="forest">Forest</option>
+                <option value="mountain">Mountain</option>
+                <option value="snow">Snow</option>
+            </select>
+            <input type="submit" value="Search"/>
+        </form>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384"></script>
+         <footer>
+            <figure id="buddyID">
+                <img src="img/buddy_verified.png" alt="CSUMB_logo"/>
+            </figure>
+            
+        </footer>
     </body>
 </html>
---
+++ practice/buddyIndex.php
@@ -0,0 +1,148 @@
+<?php
+ //print_r($_GET); //displaying all content submitted in the form using the GET method
+  $backgroundImage = "img/sea.jpg";
+    
+  if (isset($_GET['keyword'])) { //if form was submitted
+      include 'api/pixabayAPI.php';
+      echo "<h3>You searched for " . $_GET['keyword'] . "</h3>";
+      $orientation = "horizontal";
+      $keyword = $_GET['keyword'];
+      
+      if(isset($_GET['layout']))
+      {
+        $orientation = $_GET['layout'];
+      }
+      
+      if(!empty($_GET['category']))
+      {
+          $keyword = $_GET['category'];
+      }
+      
+      $imageURLs = getImageURLs($keyword, $orientation);
+      $backgroundImage = $imageURLs[array_rand($imageURLs)];
+      
+  }
+  
+  function checkCategory($category)
+  {
+      if($category == $_GET['category'])
+      {
+          echo "selected";
+      }
+  }
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lab 4: Pixabay Carousel </title>
+    </head>
+    <style>
+        @import url("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+        @import url("css/styles.css");
+        body {
+            background-image: url(<?=$backgroundImage?>);
+        }
+        
+        #carouselExampleIndicators {
+            width: 500px;
+            margin: 0 auto;
+        }
+         
+    </style>
+    <body>
+
+        <?php
+            if (!isset($_GET['keyword'])) 
+            {
+              echo "<h2> You must type a keyword or select a category </h2>";
+            }  
+        ?>
+        
+      <form method="GET">
+            <input type="text" size="20" name="keyword" placeholder="Keyword to search for" value="<?=$_GET['keyword']?>"/>
+            <input type="radio" name="layout" value="horizontal" id="hlayout"
+            <?php
+               if ($_GET['layout'] == "horizontal") 
+               {
+                 echo "checked";
+               }
+            ?>
+            >
+            <label for="hlayout"> Horizontal </label>
+            <input type="radio" name="layout" value="vertical" id="vlayout" <?($_GET['layout'] == "vertical")?"checked": "" ?>
+            <label for="vlayout"> Vertical </label>
+            
+            <select name="category">
+              <option value="" >  Select One </option> 
+              <option value="sea" <?=checkCategory('sea')?>>  Ocean </option>
+              <option <?=checkCategory('Forest')?>>  Forest </option>
+              <option <?=checkCategory('Sky')?>>  Sky </option>
+            </select>
+            
+            <input type="submit" value="Submit!"/>
+                    
+        </form>
+        
+        <?php
+          
+           if (isset($_GET['keyword'])) {
+        ?>
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[3]?>" alt="Fourth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[4]?>" alt="Fifth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[5]?>" alt="Sixth slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="<?=$imageURLs[6]?>" alt="Seventh slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+        </div>
+        
+        <?php
+            }
+        ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" /></script>
+        
+        <footer>
+            <figure id="buddyID">
+                <img src="img/buddy_verified.png" alt="CSUMB_logo"/>
+            </figure>
+        </footer>
+    </body>
+</html>
