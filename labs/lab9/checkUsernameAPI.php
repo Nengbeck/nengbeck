2818ee1f8e6d495dee4e1365a39d0c9f280079ce
Nicco Engbeck <nengbeck@csumb.edu>
2018-04-29 22:31:06 +0000

    lab 9 finished

--- labs/lab9/checkUsernameAPI.php
+++ labs/lab9/checkUsernameAPI.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../../dbConnection.php';
+/*include '../../dbConnection.php';
 
 $conn = getDatabaseConnection("c9");
 
@@ -10,14 +10,29 @@ $username = $_GET['username'];
 //$sql = "SELECT * FROM lab9_user WHERE username = '$username'";
 
 $sql = "SELECT * FROM lab9_user WHERE username = :username";
+/*$sql = "SELECT * 
+FROM lab9_user 
+WHERE username = `username`";*/
 
-
-$stmt = $conn->prepare($sql);
+/*!$stmt = $conn->prepare($sql);
 $stmt->execute( array(":username"=>$username));
 $record = $stmt->fetch(PDO::FETCH_ASSOC);
 
 //print_r($record);
 
-echo json_encode($record);
+echo json_encode($record);*/
+
+session_start();
+$_SESSION['username'] = array();
+
+$_SESSION['username'][0] = "bob";
+
+$_SESSION['username'];
+
+for($i = 0; $i < $_SESSION['username'].length(); $i++ )
+{
+    echo $_SESSION['username'][i];
+}
+
 
 ?>
